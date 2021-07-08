074b11bb8a24f46f38ca103ebffbfc844ad074fb
KhaledMosaad <khalidmosaad155@gmail.com>
2021-07-08 20:41:03 +0200

    Solve Mistake

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,8 +17,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         processesIds=new ArrayList<>();
-        for(int i=0;i<2;i++) {
-            file=new File(String.valueOf(ProcessHandle.current().pid()));
+        file=new File(String.valueOf(ProcessHandle.current().pid()));
+        for(int i=0;i<4;i++) {
             processesIds.add(newNode(file.getPath()));
         }
         shareProcessesIds();
